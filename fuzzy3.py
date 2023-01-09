a9655a24af6473fc6c19f6eba5b406bce162ddb7
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-09 05:06:02 +0200

    Tuple

--- fuzzy3.py
+++ fuzzy3.py
@@ -1,19 +1,23 @@
 def get_z(results):
     z_values = {
-        ["BL", "BL", "BL", "BL", "BL"]: "BL",
-        ["BW", "SL", "BL", "SL", "BW"]: "BL",
-        ["SW", "BL", "SL", "SL", "SW"]: "BL",
-        ["SL", "SL", "SL", "SL", "SL"]: "SL",
-        ["D", "SL", "SL", "D", "D"]: "SL",
-        ["SW", "D", "SL", "SL", "SW"]: "SL",
-        ["D", "D", "D", "D", "D"]: "D",
-        ["SL", "SW", "SW", "D", "SL"]: "D",
-        ["D", "D", "SW", "SW", "D"]: "D",
-        ["SW", "SW", "SW", "SW", "SW"]: "SW",
-        ["D", "BW", "BW", "SW", "D"]: "SW",
-        ["SL", "SW", "SW", "BW", "SL"]: "SW",
-        ["BW", "BW", "BW", "BW", "BW"]: "BW",
-        ["SL", "BW", "SW", "BW", "SL"]: "BW",
-        ["BL", "SW", "BW", "SW", "BL"]: "BW",
+        ("BL", "BL", "BL", "BL", "BL"): "BL",
+        ("BW", "SL", "BL", "SL", "BW"): "BL",
+        ("SW", "BL", "SL", "SL", "SW"): "BL",
+        ("SL", "SL", "SL", "SL", "SL"): "SL",
+        ("D", "SL", "SL", "D", "D"): "SL",
+        ("SW", "D", "SL", "SL", "SW"): "SL",
+        ("D", "D", "D", "D", "D"): "D",
+        ("SL", "SW", "SW", "D", "SL"): "D",
+        ("D", "D", "SW", "SW", "D"): "D",
+        ("SW", "SW", "SW", "SW", "SW"): "SW",
+        ("D", "BW", "BW", "SW", "D"): "SW",
+        ("SL", "SW", "SW", "BW", "SL"): "SW",
+        ("BW", "BW", "BW", "BW", "BW"): "BW",
+        ("SL", "BW", "SW", "BW", "SL"): "BW",
+        ("BL", "SW", "BW", "SW", "BL"): "BW",
     }
-    return z_values.get(results, "UNKNOWN")
+    return z_values.get(tuple(results), "UNKNOWN")
+
+
+result = get_z(["BW", "SL", "BL", "SL", "BW"])
+print(result)
