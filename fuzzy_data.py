7a66128bc8f30b2f7fdd1bfa057680383b7f452b
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-12 16:22:42 +0200

    Clean dataset

---
+++ fuzzy_data.py
@@ -0,0 +1,59 @@
+import os
+
+def get_result_name(home_score, away_score):
+    diff = home_score - away_score
+    if diff <= -3:
+        return "BL"
+    elif diff == -2 or diff == -1:
+        return "SL"
+    elif diff == 0:
+        return "D"
+    elif diff == 1 or diff == 2:
+        return "SW"
+    elif diff >= 3:
+        return "BW"
+
+# Rows for new dataset
+rows = []
+
+# Walk in the directory and get all the files
+for root, dirs, files in os.walk("data/fuzzy"):
+    for file in files:
+        if file.endswith(".csv"):
+            # open the file
+            with open(os.path.join(root, file)) as f:
+                # Get all matches for each team
+                matches = f.readlines()[1:]
+                # Create a dictionary to store the matches
+                teams = {}
+                # Loop through all the matches
+                for match in matches:
+                    # Get the teams
+                    team1 = match.split(",")[2]
+                    team2 = match.split(",")[3]
+                    # Check if the team is in the dictionary
+                    if team1 not in teams:
+                        # If not add it
+                        teams[team1] = []
+                    # Check if the team is in the dictionary
+                    if team2 not in teams:
+                        # If not add it
+                        teams[team2] = []
+                    # Add the match to the dictionary
+                    teams[team1].append(get_result_name(int(match.split(",")[4]), int(match.split(",")[5])))
+                    teams[team2].append(get_result_name(int(match.split(",")[5]), int(match.split(",")[4])))
+                rows.append(teams)
+
+print(rows)
+
+# Head to head
+h2h = {}
+for i in range(len(rows)):
+    for team1 in rows[i]:
+        for team2 in rows[i]:
+            if team1 != team2:
+                if team1 not in h2h:
+                    h2h[team1] = {}
+                # Append the result as list of 0s and 1s
+                h2h[team1][team2] = [result for result in rows[i][team1]]
+print(h2h)
--- fuzzy_dataset.py
+++ fuzzy_dataset.py
@@ -1,74 +1,51 @@
 import os
 
 # Walk in the directory and get all the files
-# for root, dirs, files in os.walk("data/fuzzy"):
-#     for file in files:
-#         if file.endswith(".csv"):
-#             # open the file
-#             with open(os.path.join(root, file)) as f:
-#                 # read the second line
-#                 line = f.readlines()[1]
-#                 # read the second column
-#                 line = line.split(",")[1]
-#                 # get the month and year and remove the day
-#                 year =line.split(" ")[0].split("/")[2]
-#                 # rename the file to year.csv
-#                 os.rename(os.path.join(root, file), os.path.join(root, year + ".csv"))
-
-def get_result_name(home_score, away_score):
-    diff = home_score - away_score
-    if diff <= -3:
-        return "BL"
-    elif diff == -2 or diff == -1:
-        return "SL"
-    elif diff == 0:
-        return "D"
-    elif diff == 1 or diff == 2:
-        return "SW"
-    elif diff >= 3:
-        return "BW"
-
-# Rows for new dataset
-rows = []
-
-# Walk in the directory and get all the files
 for root, dirs, files in os.walk("data/fuzzy"):
     for file in files:
-        if file.endswith(".csv"):
-            # open the file
-            with open(os.path.join(root, file)) as f:
-                # Get all matches for each team
-                matches = f.readlines()[1:]
-                # Create a dictionary to store the matches
-                teams = {}
-                # Loop through all the matches
-                for match in matches:
-                    # Get the teams
-                    team1 = match.split(",")[2]
-                    team2 = match.split(",")[3]
-                    # Check if the team is in the dictionary
-                    if team1 not in teams:
-                        # If not add it
-                        teams[team1] = []
-                    # Check if the team is in the dictionary
-                    if team2 not in teams:
-                        # If not add it
-                        teams[team2] = []
-                    # Add the match to the dictionary
-                    teams[team1].append(get_result_name(int(match.split(",")[4]), int(match.split(",")[5])))
-                    teams[team2].append(get_result_name(int(match.split(",")[5]), int(match.split(",")[4])))
-                rows.append(teams)
+        # Skip the files with name fuzzy.csv and fuzzy2.csv
+        if file not in ["fuzzy.csv", "fuzzy2.csv"]:
+            if file.endswith(".csv"):
+                # open the file
+                with open(os.path.join(root, file)) as f:
+                    # read the second line
+                    line = f.readlines()[1]
+                    # read the second column
+                    line = line.split(",")[1]
+                    # get the month and year and remove the day
+                    year =line.split(" ")[0].split("/")[2]
+                    # rename the file to year.csv
+                    os.rename(os.path.join(root, file), os.path.join(root, year + ".csv"))
 
-print(rows)
+order = ["95", "96", "97", "98", "99", "00", "01", 
+        "02", "03", "04", "05", "06", "07", "08", 
+        "09", "10", "11", "12", "13"]
 
-# Head to head
-h2h = {}
-for i in range(len(rows)):
-    for team1 in rows[i]:
-        for team2 in rows[i]:
-            if team1 != team2:
-                if team1 not in h2h:
-                    h2h[team1] = {}
-                # Append the result as list of 0s and 1s
-                h2h[team1][team2] = [result for result in rows[i][team1]]
-print(h2h)
+# Merge all the files into one
+for year in order:
+    # Open the file
+    with open("data/fuzzy/" + str(year) + ".csv") as f:
+        # Read all the lines
+        lines = f.readlines()
+        # Open the file
+        # Drop lines if it contains "HomeTeam"
+        lines = [line for line in lines if "HomeTeam" not in line]
+        with open("data/fuzzy/fuzzy.csv", "a") as f2:
+            # Write all the lines
+            f2.writelines(lines)
+
+# Read the file as lines
+with open("data/fuzzy/fuzzy.csv", "r") as f:
+    lines = f.readlines()
+
+# print(len(lines))
+
+# Split each line into columns
+lines = [line.split(",") for line in lines]
+# Remove the last column for each line
+lines = [line[1:6] for line in lines]
+
+# Write the lines in the file fuzzy2.csv
+with open("data/fuzzy/fuzzy2.csv", "w") as f:
+    for line in lines:
+        f.write(",".join(line) + "\n")
