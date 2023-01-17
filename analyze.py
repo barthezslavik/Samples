5eccaaa3ccd97fb1b2f245ee563012079b139988
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-17 19:32:48 +0200

    Tune

--- analyze.py
+++ analyze.py
@@ -8,7 +8,10 @@ from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb
 import process as p
 
-max_range = 50
+max_range = 2
+country = 'belgium'
+learn_period = 3
+start_year = 2010
 
 for file in glob.glob('data/*.csv'):
     os.remove(file)
@@ -16,54 +19,23 @@ for file in glob.glob('data/*.csv'):
 for file in glob.glob('data/*.png'):
     os.remove(file)
 
-# for i in range(max_range):
-#     X_train, X_test, y_train, y_test = p.data()
-#     # Logistic Regression model
-#     lr_model = LogisticRegression()
-#     lr_model.fit(X_train, y_train)
-#     y_pred = lr_model.predict(X_test)
-#     p.process(y_test, y_pred, 'regression')
-# p.plot_mean('regression')
-
-# for i in range(max_range):
-#     X_train, X_test, y_train, y_test = p.data()
-#     # Decision Tree model
-#     dt_model = DecisionTreeClassifier()
-#     dt_model.fit(X_train, y_train)
-#     y_pred = dt_model.predict(X_test)
-#     p.process(y_test, y_pred, 'decision')
-# p.plot_mean('decision')
-
 for i in range(max_range):
-    X_train, X_test, y_train, y_test = p.data()
+    X_train, X_test, y_train, y_test, test_data = p.data(country, learn_period, start_year)
     # XGBoost model
     xgb_model = xgb.XGBClassifier()
     xgb_model.fit(X_train, y_train)
     y_pred = xgb_model.predict(X_test)
-    p.process(y_test, y_pred, 'xgboost')
-p.plot_mean('xgboost')
-
-# for i in range(max_range):
-#     X_train, X_test, y_train, y_test = p.data()
-#     # Random Forest model
-#     rf_model = RandomForestClassifier()
-#     rf_model.fit(X_train, y_train)
-#     y_pred = rf_model.predict(X_test)
-#     p.process(y_test, y_pred, 'random')
-# p.plot_mean('random')
+    p.process(y_test, y_pred, 'xgboost', test_data)
+p.plot_mean('xgboost', country)
 
 for i in range(max_range):
-    X_train, X_test, y_train, y_test = p.data()
+    X_train, X_test, y_train, y_test, test_data = p.data(country, learn_period, start_year)
     # Neural Network model
     nn_model = MLPRegressor(hidden_layer_sizes=(20, 20))
     nn_model.fit(X_train, y_train)
     y_pred = nn_model.predict(X_test)
-    p.process(y_test, y_pred, 'nn')
-p.plot_mean('nn')
+    p.process(y_test, y_pred, 'nn', test_data)
+p.plot_mean('nn', country)
 
 for file in glob.glob('data/*.csv'):
     os.remove(file)
-
-# for file in glob.glob('data/*.png'):
-#     if 'accuracies' in file:
-#         os.remove(file)
--- process.py
+++ process.py
@@ -4,9 +4,9 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-def data():
+def data(country, learn_period, start_year):
     # Read in dataset
-    data = pd.read_csv('data/fuzzy/fuzzy3.csv', header=0)
+    data = pd.read_csv(f"data/{country}/fuzzy3.csv", header=0)
 
     # Create a dictionary to map outcome to integer values
     outcome_map = {'SW': 0, 'SL': 1, 'D': 2, 'BW': 3, 'BL': 4}
@@ -14,16 +14,25 @@ def data():
     # Create a new column "outcome_num" to store the mapped outcome
     data = data.replace(outcome_map)
 
+    # Get only data where date is contained in start_year + learn_period
+    original_data = data
+    data = data[data['date'].str.contains(f"{start_year}")]
+    for i in range(learn_period):
+        new_data = original_data[original_data['date'].str.contains(f"{start_year + i}")]
+        data = pd.concat([data, new_data])
+
+    test_data = data
+
     # Assign the input variables to X and the output variable to y
     X = data.drop(['date','team1','team2','y'], axis=1)
     y = data['y']
 
     # split data into train and test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, test_data
 
-def plot_mean(name):
+def plot_mean(name, country):
     # Plot accuracies.csv
     accuracies = pd.read_csv(f"data/accuracies_{name}.csv", header=None)
     accuracies.columns = ['SW', 'SL', 'D', 'BW', 'BL', 'Overall']
@@ -32,10 +41,10 @@ def plot_mean(name):
     accuracies.mean().plot(kind='bar')
     plt.xlabel('Outcome')
     plt.ylabel('Accuracy')
-    plt.savefig(f"data/mean_accuracy_{name}.png")
+    plt.savefig(f"data/{country}_{name}.png")
     plt.close()
 
-def process(y_test, y_pred, name):
+def process(y_test, y_pred, name, test_data):
     # Convert predictions to integer values
     y_pred = np.round(y_pred).astype(int)
 
@@ -109,23 +118,26 @@ def process(y_test, y_pred, name):
     accuracies.plot()
     plt.xlabel('Iterations')
     plt.ylabel('Accuracy')
-    plt.savefig(f"data/accuracies_{name}.png")
+    # plt.savefig(f"data/accuracies_{name}.png")
 
     plt.close()
 
     # restore 0 -> SW, 1 -> SL, 2 -> D, 3 -> BW, 4 -> BL
-    # y_test = y_test.replace({0: 'SW', 1: 'SL', 2: 'D', 3: 'BW', 4: 'BL'})
-    # y_pred = pd.Series(y_pred).replace({0: 'SW', 1: 'SL', 2: 'D', 3: 'BW', 4: 'BL'})
+    y_test = y_test.replace({0: 'SW', 1: 'SL', 2: 'D', 3: 'BW', 4: 'BL'})
+    y_pred = pd.Series(y_pred).replace({0: 'SW', 1: 'SL', 2: 'D', 3: 'BW', 4: 'BL'})
 
     # Merge team1, team2, test and prediction results
-    # date = pd.Series(data_test['date'])
-    # team1 = pd.Series(data_test['team1'])
-    # team2 = pd.Series(data_test['team2'])
-    # correct = pd.Series(y_test == y_pred)
-    # prediction = pd.concat([date, team1, team2, y_test, y_pred, correct], axis=1)
-    # prediction.columns = ['Date', 'Home', 'Away', 'Result', 'Prediction', 'Correct']
+    date = pd.Series(test_data['date'])
+    team1 = pd.Series(test_data['team1'])
+    team2 = pd.Series(test_data['team2'])
 
-    # Drop all BW and BL predictions
-    # prediction = prediction[prediction['Prediction'] != 'BW']
-    # prediction = prediction[prediction['Prediction'] != 'BL']
+    # correct = pd.Series(y_test == y_pred)
+
+    # # prediction = pd.concat([y_test, y_pred], axis=1)
+    # prediction = pd.concat([date, team1, team2, y_test, y_pred], axis=1)
+    # prediction.columns = ['Date', 'Home', 'Away', 'Prediction']
+
+    # # Drop all BW and BL predictions
+    # # prediction = prediction[prediction['Prediction'] != 'BW']
+    # # prediction = prediction[prediction['Prediction'] != 'BL']
     # prediction.to_csv('data/prediction.csv', index=False)
