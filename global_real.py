60dbbef6debdbc982ff7adc974f71a15a181e951
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-24 23:21:21 +0200

    Fix all

--- global_real.py
+++ global_real.py
@@ -5,8 +5,6 @@ from sklearn.neural_network import MLPRegressor
 import xgboost as xgb
 import pickle
 
-country = 'fuzzy'
-
 def format_date(date):
     date = date.replace("-", "/")
     date = date.split("/")
@@ -18,7 +16,7 @@ def format_date(date):
     return "-".join(date)
 
 # Read in dataset
-dataset = pd.read_csv(f"data/123000_train.csv", header=0)
+dataset = pd.read_csv(f"data/171000_train.csv", header=0)
 
 # Create a dictionary to map outcome to integer values
 outcome_map = {'BL': 0, 'SL': 1, 'D': 2, 'SW': 3, 'BW': 4}
@@ -45,30 +43,45 @@ y_pred3 = np.where(y_pred == 2, y_pred2, y_pred)
 df = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred3})
 # Drop all rows where y_pred == 0 and y_pred == 4
 df = df[(df['y_pred'] != 0) & (df['y_pred'] != 4)]
+
+# Replace predicted outcome with value from outcome_map
+df = df.replace({v: k for k, v in outcome_map.items()})
+
 # Add column called correct and set to 1 if y_test == y_pred
 df['correct'] = np.where(df['y_test'] == df['y_pred'], 1, 0)
 # Devide correct by total rows
 accuracy = df['correct'].sum() / df.shape[0]
 print(f'Accuracy: {accuracy}')
 
-# Show accuracy for each outcome
-for i in range(5):
-    df = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred3})
-    df = df[(df['y_pred'] == i)]
-    df['correct'] = np.where(df['y_test'] == df['y_pred'], 1, 0)
-    accuracy = df['correct'].sum() / df.shape[0]
-    # Replace i with outcome
-    i = list(outcome_map.keys())[list(outcome_map.values()).index(i)]
-    print(f'Accuracy for {i}: {accuracy}')
+# Add column win = -1
+df['win'] = -1
 
-# Add y_pred to original dataset
-dataset['prediction'] = y_pred3
-# Map y_pred back to outcome
-dataset = dataset.replace({v: k for k, v in outcome_map.items()})
-# Drop all where prediction != BW
-dataset = dataset[(dataset['prediction'] == 'D')]
+# Remove all except y_pred = D
+df = df[df['y_pred'] == 'D']
 
-# Save to csv
-dataset.to_csv(f"data/global_real.csv", index=False)
+# Add column coef and set to D from original dataset
+df['coef'] = dataset['D']
+
+# Remove all except y_pred = SL
+# df = df[df['y_pred'] == 'SL']
+
+# # Add column coef and set to A from original dataset
+# df['coef'] = dataset['A']
 
 
+# If correct == 1, set win = (coef - 1)
+df.loc[df['correct'] == 1, 'win'] = df['coef'] - 1
+
+# Sum win column
+total_win = df['win'].sum()
+print(f'Total win: {total_win}')
+
+accuracy = df['correct'].sum() / df.shape[0]
+print(f'Accuracy: {accuracy}')
+
+# Calculate roi
+roi = total_win / df.shape[0]
+print(f'ROI: {roi}')
+
+# Save data to csv
+df.to_csv('data/global_prediction.csv', index=False)
