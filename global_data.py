57dd7b9036f5261c43c72778b193fbab81ce9f68
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-24 19:34:35 +0200

    Well done

--- global_data.py
+++ global_data.py
@@ -3,9 +3,8 @@ from multiprocessing import Pool
 import pandas as pd
 
 iteration = 0;
-limit = 100;
+limit = 1000;
 dataset = pd.read_csv("data/global.csv")
-# Get first 100 lines
 dataset = dataset[:limit]
 
 def invert_result_for_away_team(result):
@@ -27,26 +26,24 @@ def clear(result):
 # History for specific team
 def history(dataset, team, date):
     # Filter the lines for the team
-    dataset = dataset[(dataset["HomeTeam"] == team) | (dataset["AwayTeam"] == team)]
+    dataset = dataset[dataset["HomeTeam"] == team]
     # Filter the lines for the date
     dataset = dataset[dataset["Date"] <= date]
-    # Invert the result for the away team
-    dataset.loc[dataset["AwayTeam"] == team, "FTR"] = dataset.loc[dataset["AwayTeam"] == team, "FTR"].apply(invert_result_for_away_team)
     # Remove all expect the result
-    dataset = dataset["FTR"]
-    print(dataset.head())
+    dataset = dataset["Y"]
+    # Return last 5 results
     return dataset[-5:]
 
 # Head to head for two teams for a specific date
 def hh(dataset, team1, team2, date):
-    # Filter the lines for the team
-    dataset = dataset[((dataset["HomeTeam"] == team1) & (dataset["AwayTeam"] == team2)) | ((dataset["HomeTeam"] == team2) & (dataset["AwayTeam"] == team1))]
+    # Filter the lines for the team1
+    dataset = dataset[dataset["HomeTeam"] == team1]
+    # Filter the lines for the team2
+    dataset = dataset[dataset["AwayTeam"] == team2]
     # Filter the lines for the date
     dataset = dataset[dataset["Date"] <= date]
-    # Invert the result for the away team
-    dataset.loc[dataset["AwayTeam"] == team1, "FTR"] = dataset.loc[dataset["AwayTeam"] == team1, "FTR"].apply(invert_result_for_away_team)
     # Remove all expect the result
-    dataset = dataset["FTR"]
+    dataset = dataset["Y"]
     # Return last 2 results
     return dataset[-2:]
 
@@ -58,28 +55,56 @@ def hh(dataset, team1, team2, date):
 def build_dataset(dataset):
     # For each line in the dataset
     for index, row in dataset.iterrows():
+        # Print the iteration
+        print("Iteration: " + str(index) + '/' + str(len(dataset)))
         # Get the history for the home team
         home_history = history(dataset, row["HomeTeam"], row["Date"])
         # Get the history for the away team
         away_history = history(dataset, row["AwayTeam"], row["Date"])
         # Get the head to head for the two teams
         hh_history = hh(dataset, row["HomeTeam"], row["AwayTeam"], row["Date"])
-        # Add the result to the dataset
-        dataset.loc[index, "x1"] = home_history.iloc[-1]
-        dataset.loc[index, "x2"] = home_history.iloc[-2]
-        dataset.loc[index, "x3"] = home_history.iloc[-3]
-        dataset.loc[index, "x4"] = home_history.iloc[-4]
-        dataset.loc[index, "x5"] = home_history.iloc[-5]
-        dataset.loc[index, "x6"] = away_history.iloc[-1]
-        dataset.loc[index, "x7"] = away_history.iloc[-2]
-        dataset.loc[index, "x8"] = away_history.iloc[-3]
-        dataset.loc[index, "x9"] = away_history.iloc[-4]
-        dataset.loc[index, "x10"] = away_history.iloc[-5]
-        dataset.loc[index, "x11"] = hh_history.iloc[-1]
-        dataset.loc[index, "x12"] = hh_history.iloc[-2]
-        dataset.loc[index, "y"] = row["FTR"]
+        # Get the head to head for the two teams
+        hh_history_away = hh(dataset, row["AwayTeam"], row["HomeTeam"], row["Date"])
+
+        # Add the history for the home team
+        if len(home_history) > 0:
+            dataset.at[index, "x1"] = home_history.iloc[-1]
+        if len(home_history) > 1:
+            dataset.at[index, "x2"] = home_history.iloc[-2]
+        if len(home_history) > 2:
+            dataset.at[index, "x3"] = home_history.iloc[-3]
+        if len(home_history) > 3:
+            dataset.at[index, "x4"] = home_history.iloc[-4]
+        if len(home_history) > 4:
+            dataset.at[index, "x5"] = home_history.iloc[-5]
+
+        # Add the history for the away team
+        if len(away_history) > 0:
+            dataset.at[index, "x6"] = away_history.iloc[-1]
+        if len(away_history) > 1:
+            dataset.at[index, "x7"] = away_history.iloc[-2]
+        if len(away_history) > 2:
+            dataset.at[index, "x8"] = away_history.iloc[-3]
+        if len(away_history) > 3:
+            dataset.at[index, "x9"] = away_history.iloc[-4]
+        if len(away_history) > 4:
+            dataset.at[index, "x10"] = away_history.iloc[-5]
+
+        # Add the head to head for the two teams
+        dataset.at[index, "x11"] = hh_history.iloc[-1]
+
+        if len(hh_history_away) > 0:
+            dataset.at[index, "x12"] = hh_history_away.iloc[-1]
+
+        # Drop row if x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, Y is null
+        if pd.isnull(dataset.at[index, "x1"]) or pd.isnull(dataset.at[index, "x2"]) or pd.isnull(dataset.at[index, "x3"]) or pd.isnull(dataset.at[index, "x4"]) or pd.isnull(dataset.at[index, "x5"]) or pd.isnull(dataset.at[index, "x6"]) or pd.isnull(dataset.at[index, "x7"]) or pd.isnull(dataset.at[index, "x8"]) or pd.isnull(dataset.at[index, "x9"]) or pd.isnull(dataset.at[index, "x10"]) or pd.isnull(dataset.at[index, "x11"]) or pd.isnull(dataset.at[index, "x12"]) or pd.isnull(dataset.at[index, "Y"]):
+            dataset = dataset.drop(index)
+
+
+    # Drop FTR
+    dataset = dataset.drop("FTR", axis=1)
+
     # Save the dataset
-    print(dataset.head())
-    dataset.to_csv("data/global_dataset.csv", index=False)
+    dataset.to_csv("data/global_train.csv", index=False)
 
 build_dataset(dataset)
