29782b198b517c60b791e7ccf05704d352816251
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-28 19:11:02 +0200

    Fix points

--- tour_dataset.py
+++ tour_dataset.py
@@ -33,62 +33,40 @@ for index, row in df.iterrows():
     df.at[index, "HomePoints"] = points.get(row["HomeTeam"], 0)
     df.at[index, "AwayPoints"] = points.get(row["AwayTeam"], 0)
 
-# Create a new column for the tour
-df["Tour"] = 0
-
-# Create a dict to store the current tour for each team
-current_tour = {}
-
-# Iterate over the rows of the dataset
-for index, row in df.iterrows():
-    current_tour[row["HomeTeam"]] = current_tour.get(row["HomeTeam"], 0) + 1
-    df.at[index, "Tour"] = current_tour[row["HomeTeam"]]
-
 # Drop all the columns except FTHG, FTAG, HomePoints, AwayPoints, Tour
-df = df[["HomeTeam", "AwayTeam", "FTHG", "FTAG", "HomePoints", "AwayPoints", "Tour"]]
+df = df[["Date", "HomeTeam", "AwayTeam", "FTHG", "FTAG", "HomePoints", "AwayPoints"]]
 
 # Save the dataset
 df.to_csv("data/tour/E0_1.csv", index=False)
 
+# Transorm Date to_datetime
+df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+
+# Sort by Date
+df = df.sort_values(by=['Date'])
+
 # Drop the unnecessary columns
 df = df.drop(["FTHG", "FTAG"], axis=1)
 
-# Create a new dataframe for each tour
-tours = df["Tour"].unique()
-dfs = {}
-for tour in tours:
-    dfs[tour] = df[df["Tour"] == tour]
-    dfs[tour] = dfs[tour].drop(["Tour"], axis=1)
+# Create a new dataset with points for each team
+df_points = pd.DataFrame(columns=["Date", "Team", "Points"])
 
-print(df.head())
+# Iterate over the rows of the dataset
+for index, row in df.iterrows():
+    # Create a new row for the home team
+    df_points = df_points.append({
+        "Date": row["Date"],
+        "Team": row["HomeTeam"],
+        "Points": row["HomePoints"]
+    }, ignore_index=True)
+    # Create a new row for the away team
+    df_points = df_points.append({
+        "Date": row["Date"],
+        "Team": row["AwayTeam"],
+        "Points": row["AwayPoints"]
+    }, ignore_index=True)
 
-# Create a copy of the original dataframe
-df_standings = df.copy()
+# Group by Date and Team
+df_points = df_points.groupby(["Team","Date"]).sum().reset_index()
 
-# Group the data by tour
-grouped = df_standings.groupby('Tour')
-
-# Calculate total points for each team
-df_standings['TotalPoints'] = df_standings['HomePoints'] + df_standings['AwayPoints']
-
-df_standings['Standing'] = None
-
-# Create a new column for team standing
-for name, group in grouped:
-    group = group.sort_values(by=['TotalPoints'], ascending=False)
-    group['Standing'] = range(1, len(group) + 1)
-    df_standings.loc[group.index] = group
-
-# pivot the data
-df_standings_pivot = df_standings.pivot_table(values=['TotalPoints','Standing'], index=['HomeTeam','Tour'],aggfunc='first')
-df_standings_pivot.reset_index(inplace=True)
-df_standings_pivot.rename(columns={'HomeTeam':'TeamName'},inplace=True)
-
-df_standings_pivot_away = df_standings.pivot_table(values=['TotalPoints','Standing'], index=['AwayTeam','Tour'],aggfunc='first')
-df_standings_pivot_away.reset_index(inplace=True)
-df_standings_pivot_away.rename(columns={'AwayTeam':'TeamName'},inplace=True)
-
-# concatenate the result from home and away
-df_standings_all = pd.concat([df_standings_pivot,df_standings_pivot_away],ignore_index=True)
-
-print(df_standings_all.head(50))
+print(df_points.head(50))
