249815b212fa721125fe05f6401c3bd1f6b4a47a
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-01-31 13:18:11 +0200

    Fix: Drop NA values and evaluate model accuracy

--- titanic2.py
+++ titanic2.py
@@ -8,6 +8,9 @@ import xgboost as xgb
 train = pd.read_csv("data/titanic/train.csv")
 test = pd.read_csv("data/titanic/test.csv")
 
+# Drop NA values
+train = train.dropna()
+
 # Drop the columns that are not needed
 # ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 X = train.drop(['PassengerId', 'Survived', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis=1)
@@ -36,6 +39,78 @@ xgb_model.fit(X_train, y_train)
 y_pred = xgb_model.predict(X_test)
 
 print("XGBoost model")
+
+# Accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {}".format(accuracy))
+
+# Use neural network
+from sklearn.neural_network import MLPClassifier
+
+# Create a neural network
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)
+
+# Train the model
+mlp.fit(X_train, y_train)
+
+# Predict the outcome
+y_pred = mlp.predict(X_test)
+
+print("Neural network model")
+
+# Accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {}".format(accuracy))
+
+# Use logistic regression
+from sklearn.linear_model import LogisticRegression
+
+# Create a logistic regression model
+logreg = LogisticRegression()
+
+# Train the model
+logreg.fit(X_train, y_train)
+
+# Predict the outcome
+y_pred = logreg.predict(X_test)
+
+print("Logistic regression model")
+
+# Accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {}".format(accuracy))
+
+# Support Vector Machines
+from sklearn.svm import SVC
+
+# Create a support vector classifier
+svc = SVC()
+
+# Train the model
+svc.fit(X_train, y_train)
+
+# Predict the outcome
+y_pred = svc.predict(X_test)
+
+print("Support vector machine model")
+
+# Accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {}".format(accuracy))
+
+# Use decision tree
+from sklearn.tree import DecisionTreeClassifier
+
+# Create a decision tree classifier
+dtree = DecisionTreeClassifier()
+
+# Train the model
+dtree.fit(X_train, y_train)
+
+# Predict the outcome
+y_pred = dtree.predict(X_test)
+
+print("Decision tree model")
 
 # Accuracy score
 accuracy = accuracy_score(y_test, y_pred)
