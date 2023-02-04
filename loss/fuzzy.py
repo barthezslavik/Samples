587696089af05977c51ee1136f6ec7c684c91e73
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-02-04 22:57:20 +0200

    Fix: Add support for two teams comparison and new dataset #TwoTeams

--- loss/fuzzy.py
+++ loss/fuzzy.py
@@ -5,17 +5,14 @@ import matplotlib.pyplot as plt
 
 global_data = []
 
-def get_all_matches(dataset, team):
+def get_matches(dataset, team):
     return dataset[(dataset['HomeTeam'] == team) | (dataset['AwayTeam'] == team)]
 
-def teams(dataset):
-    return dataset['HomeTeam'].unique()
-
-def teams2(dataset):
-    return dataset['Team'].unique()
+def get_all_matches(dataset, home, away):
+    return dataset[(dataset['HomeTeam'] == home) | (dataset['AwayTeam'] == away) | (dataset['HomeTeam'] == away) | (dataset['AwayTeam'] == home)]
 
 def get_points(dataset, team, date):
-    matches = get_all_matches(dataset, team)
+    matches = get_matches(dataset, team)
     points = 0
     for i in range(len(matches)):
         if matches.iloc[i]['Date'] < date:
@@ -46,34 +43,45 @@ def reverse_outcome_name(outcome):
 
 # Get outcome name
 def get_outcome_name(diff):
-    if diff < -2:
+    if diff <= -2:
         return 'BL'
-    elif diff >= -2 and diff < -1:
+    elif diff == -2 or diff == -1:
         return 'SL'
     elif diff == 0:
         return 'D'
-    elif diff > 1 and diff <= 2:
+    elif diff == 1 or diff == 2:
         return 'SW'
-    elif diff > 2:
+    elif diff >= 2:
         return 'BW'
 
 # Get last 5 matches
 def get_last_5(dataset, team, date):
-    matches = get_all_matches(dataset, team)
+    matches = get_matches(dataset, team)
     matches = matches[matches['Date'] < date]
     matches = matches.tail(5)
-    if (len(matches)) > 4:
-        print("Last 5 matches for ", team, " at ", date)
-        print(matches)
-        exit()
-    return matches
+    names = []
+    for match in matches.iterrows():
+        if match[1]['HomeTeam'] == team:
+            names.append(get_outcome_name(match[1]['FTHG'] - match[1]['FTAG']))
+        elif match[1]['AwayTeam'] == team:
+            names.append(get_outcome_name(match[1]['FTAG'] - match[1]['FTHG']))
+
+    return names
 
 # Get last 2 matches head to head
 def get_last_2_head_to_head(dataset, team1, team2, date):
-    matches = dataset[(dataset['HomeTeam'] == team1) & (dataset['AwayTeam'] == team2)]
+    matches = dataset[(dataset['HomeTeam'] == team1) & (dataset['AwayTeam'] == team2) | (dataset['HomeTeam'] == team2) & (dataset['AwayTeam'] == team1)]
     matches = matches[matches['Date'] < date]
     matches = matches.tail(2)
-    return matches
+    names = []
+    for match in matches.iterrows():
+        if match[1]['HomeTeam'] == team1:
+            names.append(get_outcome_name(match[1]['FTHG'] - match[1]['FTAG']))
+        elif match[1]['AwayTeam'] == team1:
+            names.append(get_outcome_name(match[1]['FTAG'] - match[1]['FTHG']))
+    return names
+
+dfs = []
 
 # Walk in the directory and get all the files
 for root, dirs, files in os.walk("data/discovery"):
@@ -85,47 +93,49 @@ for root, dirs, files in os.walk("data/discovery"):
             # Get first row of the dataframe
             first_row = df.iloc[1]
             date = first_row['Date'].split('/')[2]
-            if date == '13' or date == '2013':
-                print("Processing file: ", file, " - ", first_row['Div'])
+            years = ['13', '2013', '14', '2014']
+            if date in years:
                 if first_row['Div'] == 'E0':
-                    # Create new dataset
-                    d = pd.DataFrame(columns=['Date', 'Team', 'Tour', 'Points'])
-                    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%y')
-                    # For each team
-                    for team in teams(df):
-                        # Get all matches for that team
-                        matches = get_all_matches(df, team)
-                        # For each match
-                        for i in range(len(matches)):
-                            # Get the date of the match
-                            date = matches.iloc[i]['Date']
-                            # Get the position of the team at that date
-                            tour = len(matches[matches['Date'] < date])
-                            # Points of the team
-                            points = get_points(matches, team, date)
-                            # Last 5 matches
-                            last_5 = get_last_5(matches, team, date)
-                            # Create a new row
-                            new_row = pd.DataFrame([[date, team, tour, points]], columns=['Date', 'Team', 'Tour', 'Points'])
-                            # Append the row to the dataset
-                            d = d.append(new_row, ignore_index=True)
+                    print(os.path.join(root, file))
+                    # Merge the dataframes
+                    dfs.append(df)
 
-                    # Remove all except the tour, poins and position
-                    d = d.drop(['Date', 'Team'], axis=1)
+# Merge all the dataframes
+dfs = pd.concat(dfs)
 
-                    # Drop first 5 tours
-                    d = d[d['Tour'] > 6]
+# Create new dataset
+d = pd.DataFrame(columns=['Date', 'Home', 'Away', 'Tour', 'Points', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'Y'])
+dfs['Date'] = pd.to_datetime(dfs['Date'], format='%d/%m/%y')
+# For each team
+for match in dfs.iterrows():
+    home = match[1]['HomeTeam']
+    away = match[1]['AwayTeam']
+    print(match[1]['Date'], home, '-', away)
+    # Get the date of the match
+    date = match[1]['Date']
+    # Points
+    home_points = get_points(dfs, home, date)
+    away_points = get_points(dfs, away, date)
+    # Last 5 matches
+    home_last_5 = get_last_5(dfs, home, date)
+    away_last_5 = get_last_5(dfs, away, date)
+    # Last 2 matches head to head
+    head_to_head = get_last_2_head_to_head(dfs, home, away, date)
+    # Result
+    result = get_outcome_name(match[1]['FTHG'] - match[1]['FTAG'])
+    # Create new row
+    if (len(home_last_5) == 5) and (len(away_last_5) == 5) and (len(head_to_head) == 2):
+        new_row = pd.DataFrame([[date, home, away, home_points, 
+                                home_last_5[0], home_last_5[1], home_last_5[2], home_last_5[3], home_last_5[4], 
+                                away_last_5[0], away_last_5[1], away_last_5[2], away_last_5[3], away_last_5[4], 
+                                head_to_head[0], head_to_head[1], result]], 
+                                columns=['Date', 'Home', 'Away', 'Points', 'X1', 'X2', 'X3', 'X4', 'X5',
+                                        'X6', 'X7', 'X8', 'X9', 'X10', 'X11', 'X12', 'Y'])
+        # Append the row to the dataset
+        global_data.append(new_row)
 
-                    # Drop all where the next position is NaN
-                    d = d.dropna()
-
-                    global_data.append(d)
-
-# Concatenate all the datasets
+# Merge all the dataframes
 global_data = pd.concat(global_data)
-
-# Add column Changed = 2 if the Next Position > Position; 1 if the Next Position == Position; 0 otherwise
-# global_data['Changed'] = global_data.apply(lambda row: 2 if row['Next Position'] > row['Position'] else 1 if row['Next Position'] == row['Position'] else 0, axis=1)
 
 # Save the dataset
 global_data.to_csv('data/good/fuzzy.csv', index=False)
