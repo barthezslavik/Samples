e56954c4ddca531a82954cbe2c4b2fed3f4c42f0
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-02-05 03:29:04 +0200

    Current

--- loss/custom_loss_xgb.py
+++ loss/custom_loss_xgb.py
@@ -20,19 +20,16 @@ data = pd.read_csv('data/good/fuzzy.csv')
 # Replace BL, SL, D, SW, BW with 1, 2, 3, 4, 5
 data = data.replace(['BW', 'SW', 'D', 'SL', 'BL'], [0, 0, 1, 2, 2])
 
-H_min = 1.96
-D_min = 2.92
-A_min = 2.35
-
 # Split data into features and labels
 X = data.drop(['Y','Date','Home','Away'], axis=1)
 y = data['Y']
 
 # Split data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # XGBoost model
-model = xgb.XGBClassifier(objective='multi:softprob', num_class=3, tree_method='exact')
+# model = xgb.XGBClassifier(objective='multi:softprob', num_class=3, tree_method='exact')
+model = xgb.XGBClassifier()
 
 # Fit the model
 model.fit(X_train, y_train)
@@ -44,13 +41,25 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
 
+accuracies = []
+
 # Show accuracy for each outcome
 for i in range(3):
     df = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
     df = df[(df['y_pred'] == i)]
     df['correct'] = np.where(df['y_test'] == df['y_pred'], 1, 0)
     accuracy = df['correct'].sum() / df.shape[0]
+    accuracies.append(accuracy)
     print(f'Accuracy for {i}: {accuracy}')
+
+k = 0
+H_min = 1/accuracies[0] + k
+D_min = 1/accuracies[1] + k
+A_min = 1/accuracies[2] + k
+
+print(f'H_min: {H_min}')
+print(f'D_min: {D_min}')
+print(f'A_min: {A_min}')
 
 # Merge with original dataset
 df = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
@@ -62,28 +71,29 @@ df['correct'] = np.where(df['y_test'] == df['y_pred'], 1, 0)
 # Add column win = -1
 df['win'] = 0
 
-# Set win = (H - 1) if y_pred == 0 and H > 1.96 else -1
-df.loc[(df['y_pred'] == 0) & (df['H'] > 1.96), 'win'] = df['H'] - 1
+# Set win = (H - 1) if y_pred == 0 and H > H_min else -1
+df.loc[(df['y_pred'] == 0) & (df['H'] > H_min), 'win'] = df['H'] - 1
 
-# Set win = (D - 1) if y_pred == 1 and D > 2.92 else -1
-df.loc[(df['y_pred'] == 1) & (df['D'] > 2.92), 'win'] = df['D'] - 1
+# Set win = (D - 1) if y_pred == 1 and D > D_min else -1
+df.loc[(df['y_pred'] == 1) & (df['D'] > D_min), 'win'] = df['D'] - 1
 
-# Set win = (A - 1) if y_pred == 2 and A > 2.35 else -1
-df.loc[(df['y_pred'] == 2) & (df['A'] > 2.35), 'win'] = df['A'] - 1
+# Set win = (A - 1) if y_pred == 2 and A > A_min else -1
+df.loc[(df['y_pred'] == 2) & (df['A'] > A_min), 'win'] = df['A'] - 1
 
 # Set win = -1 if correct == 0
 df.loc[df['correct'] == 0, 'win'] = -1
 
-# Set win = 0 if y_pred = 0 and H < 1.96
-df.loc[(df['y_pred'] == 0) & (df['H'] < 1.96), 'win'] = 0
+# Set win = 0 if y_pred = 0 and H < H_min
+df.loc[(df['y_pred'] == 0) & (df['H'] < H_min), 'win'] = 0
 
-# Set win = 0 if y_pred = 1 and D < 2.92
-df.loc[(df['y_pred'] == 1) & (df['D'] < 2.92), 'win'] = 0
+# Set win = 0 if y_pred = 1 and D < D_min
+df.loc[(df['y_pred'] == 1) & (df['D'] < D_min), 'win'] = 0
 
-# Set win = 0 if y_pred = 2 and A < 2.35
-df.loc[(df['y_pred'] == 2) & (df['A'] < 2.35), 'win'] = 0
+# Set win = 0 if y_pred = 2 and A < A_min
+df.loc[(df['y_pred'] == 2) & (df['A'] < A_min), 'win'] = 0
 
-# print(df)
+# print(df.head(20))
+print(df.tail(50))
 
 # Drop rows where win == 0
 df = df[df['win'] != 0]
