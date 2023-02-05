38dd88ef86136e91b1636d2670b1a12d2679b110
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-02-05 06:42:17 +0200

    Fix: Added logistic regression model and relevant columns for prediction

--- loss/expected.py
+++ loss/expected.py
@@ -27,8 +27,17 @@ y = data['Expected']
 # Split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
-# XGBoost model
-model = xgb.XGBClassifier()
+# # XGBoost model
+# model = xgb.XGBClassifier()
+
+# # Fit the model
+# model.fit(X_train, y_train)
+
+# # Make predictions for test data
+# y_pred = model.predict(X_test)
+
+# SVM model
+model = LogisticRegression()
 
 # Fit the model
 model.fit(X_train, y_train)
@@ -47,39 +56,41 @@ df = df.merge(data[['Date','Home','Away','H','D','A']], left_index=True, right_i
 # Merge Y from original dataset
 df = df.merge(data[['Y']], left_index=True, right_index=True)
 
-# Add column win = -1
-df['win'] = 0
+# Add column bet = ''
+df['Bet'] = ''
 
-# if y_pred == y_test == 1 and H < A, win = (H - 1)
-df['win'] = np.where((df['y_pred'] == 1) & (df['y_test'] == 1) & (df['H'] < df['A']), df['H'] - 1, df['win'])
+# Set Bet = 0 if y_pred == 1 and H < A
+df['Bet'] = np.where((df['y_pred'] == 1) & (df['H'] < df['A']), 0, df['Bet'])
 
-# if y_pred == y_test == 1 and H > A, win = (A - 1)
-df['win'] = np.where((df['y_pred'] == 1) & (df['y_test'] == 1) & (df['H'] > df['A']), df['A'] - 1, df['win'])
+# Set Bet = 2 if y_pred == 1 and A < H
+df['Bet'] = np.where((df['y_pred'] == 1) & (df['A'] < df['H']), 2, df['Bet'])
 
-# if y_pred == y_test == 0 and Y == 0 and A < H, win = (H - 1)
-df['win'] = np.where((df['y_pred'] == 0) & (df['y_test'] == 0) & (df['Y'] == 0) & (df['H'] > df['A']), df['H'] - 1, df['win'])
+# Set Bet = 0 if y_pred == 0 and H > A
+df['Bet'] = np.where((df['y_pred'] == 0) & (df['H'] > df['A']), 0, df['Bet'])
 
-# if y_pred == y_test == 0 and Y == 2 and H < A, win = (A - 1)
-df['win'] = np.where((df['y_pred'] == 0) & (df['y_test'] == 0) & (df['Y'] == 2) & (df['H'] < df['A']), df['A'] - 1, df['win'])
+# Set Bet = 2 if y_pred == 0 and A > H
+df['Bet'] = np.where((df['y_pred'] == 0) & (df['A'] > df['H']), 2, df['Bet'])
 
-# if Y == 1, win = -1
-df['win'] = np.where(df['Y'] == 1, -1, df['win'])
+# Drop all rows where y_pred == 0
+df = df[df['Bet'] != 0]
 
-# if y_pred != y_test, win = -1
-df['win'] = np.where(df['y_pred'] != df['y_test'], -1, df['win'])
+# Add column Win = 0
+df['Win'] = -1
 
+# Set win = (H - 1) if Y == Bet and Bet == 0
+df['Win'] = np.where((df['Y'] == df['Bet']) & (df['Bet'] == 0), df['H'] - 1, df['Win'])
 
-# Drop all rows where Y != 0
-# df = df[df['Y'] == 2]
+# Set win = (D - 1) if Y == Bet and Bet == 1
+df['Win'] = np.where((df['Y'] == df['Bet']) & (df['Bet'] == 2), df['A'] - 1, df['Win'])
 
-print(df.head(20))
+print(df.head(50))
 
 # Total bets
 total_bets = df.shape[0]
 print(f'Total bets: {total_bets}')
 
 # Calculate profit
-profit = df['win'].sum()
+profit = df['Win'].sum()
 print(f'Profit: {profit}')
 
 # Calculate profit per game
